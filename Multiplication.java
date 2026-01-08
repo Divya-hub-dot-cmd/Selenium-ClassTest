88ae7084e5ed6f6f0239e791162e6d74882a48c2
Divya-hub-dot-cmd <dhivyavelumani003@gmail.com>
2026-01-08 14:54:17 +0530

    Questio12

---
+++ Multiplication.java
@@ -0,0 +1,14 @@
+package test.day1;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=8;
+		for(int i=1;i<=10;i++) {
+			System.out.println( (num*i));
+		}
+
+	}
+
+}
