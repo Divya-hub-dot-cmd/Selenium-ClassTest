0474cb2c571352720f57b4ae7cc28d4c80b3d28d
Divya-hub-dot-cmd <dhivyavelumani003@gmail.com>
2026-01-08 14:54:49 +0530

    Question13

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+package test.day1;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int var=1;
+		for(int i=5;i>=1;i--) {
+			var=var*i;
+			System.out.println("The factorial of a number is:"+var);
+			}
+
+	}
+
+}
